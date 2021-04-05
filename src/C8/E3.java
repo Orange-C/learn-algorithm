7de318b8c4af9b3e47f6176535c13f6c2205276d
Orange-C <oraaangecolin@gmail.com>
2021-04-05 16:36:26 +0800

    update

--- src/C8/E3.java
+++ src/C8/E3.java
@@ -4,7 +4,13 @@ public class E3 {
 
     public static void main(String[] args) throws Exception {
         DisjointSet ds = new DisjointSet(10);
-        
+        ds.print();
+        ds.union(0, 1);
+        ds.print();
+        ds.union(2, 3);
+        ds.print();
+        ds.union(1, 2);
+        ds.print();
     }
 }
 
@@ -29,19 +35,24 @@ class DisjointSet {
     public int union(int a, int b) {
         int aroot = find(a);
         int broot = find(b);
+        
+        if (aroot == broot) return aroot;
 
-        if (s[aroot] > s[broot]) {
-            s[broot] = aroot;
-            s[aroot] += s[broot];
+        if (s[aroot] < s[broot]) {
+            s[aroot] += s[broot]; // add size
+            s[broot] = aroot; // set root
             return aroot;
         } else {
-            s[aroot] = broot;
             s[broot] += s[aroot];
+            s[aroot] = broot;
             return broot;
         }
     }
 
     public void print() {
-        for(int i = 0; i < s)
+        for(int i = 0; i < s.length; i++) {
+            System.out.printf("%d=%d, ", i, s[i]);
+        }
+        System.out.println();
     }
 }
